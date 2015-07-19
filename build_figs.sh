fd4af564f270aa3f0ff1c84ab75846644ef55b73
Tobias Schlatter <tobias@meisch.ch>
2015-07-18 18:24:53 -0700

    First version of full pipelines

---
+++ build_figs.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+doConvert() {
+    for i in *.svg; do
+	inkscape -D -z --file=$i --export-pdf=${i%.svg}.pdf;
+    done
+}
+
+cd $(dirname $0)/icons/files
+doConvert
+
+cd ../components
+doConvert
